942557d53fcf8736be1ee95d517cb660eb38a91a
Aryan0199 <63339536+Aryan0199@users.noreply.github.com>
2020-05-01 09:41:55 +0530

    Create pointarr.cpp

---
+++ pointarr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5],*p;
+    p=arr;
+    cout<<"Enter elements:";
+    for (int i = 0; i < 5; ++i) {
+            cin>>arr[i];
+    }
+    cout<<"You entered:\n";
+    for (int i = 0; i < 5; ++i) {
+        cout<<*p<<endl;
+        p++;
+    }
+
+}
